054a587056bb616ecf41306cb44b753195b23645
CaroleblD <endlesslonelinesselln@gmail.com>
2024-08-09 19:07:58 +0500

    feat: add code in index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let morseWord = ''
+
+    
+    for (let i = 0; i < expr.length; i += 10) {
+
+        if (expr.slice(i, (i + 10)) === '**********') {
+            morseWord += ' '
+            continue
+        }
+
+        if (expr.slice(i, (i + 10)) === undefined) {
+            continue
+        }
+
+
+        let binLetter = expr.slice(i, (i + 10))
+
+        let morseLetter = ''
+
+        for (let x = 0; x < binLetter.length; x += 2) {
+
+            let morse = ''
+            
+            if (binLetter.slice(x, (x + 2)) === '00') morse += ''
+            if (binLetter.slice(x, (x + 2)) === '10') morse += '.'
+            if (binLetter.slice(x, (x + 2)) === '11') morse += '-'
+
+            morseLetter += morse
+
+        }
+
+        let letter = MORSE_TABLE[morseLetter]
+
+        morseWord += letter
+
+    }
+
+    return morseWord
+
 }
 
 module.exports = {
